701063daed8653a5678334957f0109d59f811fd0
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2014-08-06 14:01:40 +0900

    add dataOriented

---
+++ sec02/tableLikeDate.js
@@ -0,0 +1,6 @@
+var _ = require('underscore');
+var libraty = [
+  {title: "SICP", isbn: "02347289423", ed: 1},
+  {title: "SICP", isbn: "02347289356", ed: 2},
+  {title: "Joy of Closure", isbn: "1234567890", ed: 1}
+];
