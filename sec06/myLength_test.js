9ba592afc1742f9f4c711d9e8a52670245d63f9d
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2014-08-07 22:50:02 +0900

    add myLength, myLength_test

---
+++ sec06/myLength_test.js
@@ -0,0 +1,24 @@
+var sut = require('./myLength.js');
+
+var should = require('should');
+var _ = require('underscore');
+
+describe('myLength', function() {
+  it('[]を渡すと0を返す', function() {
+    sut.myLength([]).should.equal(0);
+  });
+
+  it('[1]を渡すと1を返す', function() {
+    sut.myLength([1]).should.equal(1);
+  });
+
+  it('[\'a\', \'b\', \'c\']を渡すと3を返す', function() {
+    sut.myLength(['a', 'b', 'c']).should.equal(3);
+  });
+
+  it('_.range(100)を渡すと100を返す', function() {
+    sut.myLength(_.range(100)).should.equal(100);
+  });
+
+});
+
