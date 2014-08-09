a0626ecff2dfbb59daffc2a0cff294dd6a94af53
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2014-08-09 20:30:45 +0900

    refactor fail

--- sec01/fail.js
+++ sec01/fail.js
@@ -1,15 +1,21 @@
+exports.fail = fail;
+exports.warn = warn;
+exports.note = note;
 
-exports.fail = function (thing) {
+
+function fail (thing) {
   throw new Error(thing);
 }
 
 // function warn(thing) {
 //   console.log(["警告：", thing].join(''));
 // }
-exports.warn = function (str) {
+function (str) {
   console.log("有効な年齢ではなさそうなものが入力されている。");
 }
+
+
 // function note(thing) {
 //   console.log(["情報：", thing].join(''));
 // }
-exports.note = function () {}
+function note () {}
