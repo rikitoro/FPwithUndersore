59d6a924dda91644003c7bcc939e5c9c9bacb5e9
yoshiaki <yoshiaki@sendai-nct.ac.jp>
2014-08-19 16:24:05 +0900

    add mapcat_test

--- sec02/mapcat.js
+++ sec02/mapcat.js
@@ -1,5 +1,10 @@
+exports.cat = cat;
+exports.mapcat = mapcat;
+exports.interpose = interpose;
+
 var _ = require('underscore');
 var existy = require('./truthy.js').existy;
+var construct = require('./construct.js').construct;
 
 function cat() {
   var head = _.first(arguments);
@@ -9,25 +14,13 @@ function cat() {
     return [];
 }
 
-////
-var p = console.log;
-p( cat([1, [2, 3]], [4, 5], [6, 7, 8]) );
-
-///////////////////////////////////////
-function construct(head, tail) {
-  return cat([head], _.toArray(tail))
-}
-
-p ( construct(42, [1, 2, 3]) );
-///////////////////////////////////////
-
 function mapcat(fun, coll) {
   return cat.apply(null, _.map(coll, fun));
 }
 
-p ( mapcat(function(e){
-  return construct(e, ["*", "**"])
-}, [1, 2, 3]) );
+// p ( mapcat(function(e){
+//   return construct(e, ["*", "**"])
+// }, [1, 2, 3]) );
 
 ///////////////////////////////////////
 function butlast(coll) {
@@ -40,4 +33,4 @@ function interpose(inter, coll) {
   }, coll));
 }
 
-p( interpose("*", [1,2,3]) );
+// p( interpose("*", [1,2,3]) );
